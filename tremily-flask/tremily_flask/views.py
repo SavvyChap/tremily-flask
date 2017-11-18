252c2acba7bb8b289f1f5fd45184b724ee15cee6
august1073 <augustmoney81@gmail.com>
2017-11-18 13:54:38 -0600

    just a touch up or two before having to step away

--- tremily-flask/tremily_flask/views.py
+++ tremily-flask/tremily_flask/views.py
@@ -12,7 +12,7 @@ def home():
     """Renders the home page."""
     return render_template(
         'index.html',
-        title='Home Page',
+        title='Travis and Emily\'s Big Day',
         year=datetime.now().year,
     )
 
